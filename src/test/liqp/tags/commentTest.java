e00cea71cb990dae4a1a12dba0a2c14a1cfbb22c
Bart Kiers <bkiers@gmail.com>
2013-01-12 08:34:18 +0100

    Added some unit tests.

---
+++ src/test/liqp/tags/commentTest.java
@@ -0,0 +1,28 @@
+package liqp.tags;
+
+import liqp.Template;
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class commentTest {
+
+    @Test
+    public void renderTest() throws RecognitionException {
+
+        String[][] tests = {
+                { "{% comment %}ABC{% endcomment %}", "" },
+                { "A{% comment %}B{% endcomment %}C", "AC" }
+        };
+
+        for(String[] test : tests) {
+
+            Template template = Template.parse(test[0]);
+            String rendered = String.valueOf(template.render());
+
+            assertThat(rendered, is(test[1]));
+        }
+    }
+}
---
+++ src/test/liqp/tags/rawTest.java
@@ -0,0 +1,28 @@
+package liqp.tags;
+
+import liqp.Template;
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class rawTest {
+
+    @Test
+    public void renderTest() throws RecognitionException {
+
+        String[][] tests = {
+                { "{% raw %}{% endraw %}", "" },
+                { "{% raw %}{{a|b}}{% endraw %}", "{{a|b}}" }
+        };
+
+        for(String[] test : tests) {
+
+            Template template = Template.parse(test[0]);
+            String rendered = String.valueOf(template.render());
+
+            assertThat(rendered, is(test[1]));
+        }
+    }
+}

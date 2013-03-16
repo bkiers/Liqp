ca2d3bbeb1c37b2652f5e654d6579f851afbd558
Bart Kiers <bkiers@gmail.com>
2013-03-16 21:33:31 +0100

    Added extra 'size' unit tests from the original Liquid unit test suite.

--- src/test/liqp/filters/SizeTest.java
+++ src/test/liqp/filters/SizeTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertThat;
 public class SizeTest {
 
     @Test
-    public void applyTest() throws RecognitionException {
+    public void sizeTest() throws RecognitionException {
 
         String json = "{ \"n\" : [1,2,3,4,5] }";
 
@@ -31,4 +31,21 @@ public class SizeTest {
             assertThat(rendered, is(test[1]));
         }
     }
+
+	/*
+	 * def test_size
+     *   assert_equal 3, @filters.size([1,2,3])
+     *   assert_equal 0, @filters.size([])
+     *   assert_equal 0, @filters.size(nil)
+     * end
+	 */
+	@Test
+	public void sizeTestLiquid() {
+
+        final String tagName = "size";
+
+        assertThat(Filter.getFilter(tagName).apply(new Integer[]{1, 2, 3}), is((Object)3));
+        assertThat(Filter.getFilter(tagName).apply(new Object[0]), is((Object)0));
+        assertThat(Filter.getFilter(tagName).apply(null), is((Object)0));
+	}
 }

9d28b68efa7586f66174fbc538dc5369a2aa3101
Bart Kiers <bkiers@gmail.com>
2013-03-18 21:36:14 +0100

    Added extra 'date'-filter unit tests from the original Liquid unit test suite. Fixed some bugs in the 'date'-filter.

--- src/main/liqp/filters/Date.java
+++ src/main/liqp/filters/Date.java
@@ -1,84 +1,22 @@
 package liqp.filters;
 
 import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
 class Date extends Filter {
+
+    private static Locale locale = Locale.ENGLISH;
+    private static Set<String> datePatterns = new HashSet<String>();
 
     private final static java.util.Map<Character, SimpleDateFormat> LIQUID_TO_JAVA_FORMAT =
             new java.util.HashMap<Character, SimpleDateFormat>();
 
     static {
-
-        // %% - Literal ``%'' character
-        LIQUID_TO_JAVA_FORMAT.put('%', new SimpleDateFormat("%"));
-
-        // %a - The abbreviated weekday name (``Sun'')
-        LIQUID_TO_JAVA_FORMAT.put('a', new SimpleDateFormat("EEE"));
-
-        // %A - The  full  weekday  name (``Sunday'')
-        LIQUID_TO_JAVA_FORMAT.put('A', new SimpleDateFormat("EEEE"));
-
-        // %b - The abbreviated month name (``Jan'')
-        LIQUID_TO_JAVA_FORMAT.put('b', new SimpleDateFormat("MMM"));
-        LIQUID_TO_JAVA_FORMAT.put('h', new SimpleDateFormat("MMM"));
-
-        // %B - The  full  month  name (``January'')
-        LIQUID_TO_JAVA_FORMAT.put('B', new SimpleDateFormat("MMMM"));
-
-        // %c - The preferred local date and time representation
-        LIQUID_TO_JAVA_FORMAT.put('c', new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy"));
-
-        // %d - Day of the month (01..31)
-        LIQUID_TO_JAVA_FORMAT.put('d', new SimpleDateFormat("dd"));
-
-        // %H - Hour of the day, 24-hour clock (00..23)
-        LIQUID_TO_JAVA_FORMAT.put('H', new SimpleDateFormat("HH"));
-
-        // %I - Hour of the day, 12-hour clock (01..12)
-        LIQUID_TO_JAVA_FORMAT.put('I', new SimpleDateFormat("hh"));
-
-        // %j - Day of the year (001..366)
-        LIQUID_TO_JAVA_FORMAT.put('j', new SimpleDateFormat("DDD"));
-
-        // %m - Month of the year (01..12)
-        LIQUID_TO_JAVA_FORMAT.put('m', new SimpleDateFormat("MM"));
-
-        // %M - Minute of the hour (00..59)
-        LIQUID_TO_JAVA_FORMAT.put('M', new SimpleDateFormat("mm"));
-
-        // %p - Meridian indicator (``AM''  or  ``PM'')
-        LIQUID_TO_JAVA_FORMAT.put('p', new SimpleDateFormat("a"));
-
-        // %S - Second of the minute (00..60)
-        LIQUID_TO_JAVA_FORMAT.put('S', new SimpleDateFormat("ss"));
-
-        // %U - Week  number  of the current year,
-        //      starting with the first Sunday as the first
-        //      day of the first week (00..53)
-        LIQUID_TO_JAVA_FORMAT.put('U', new SimpleDateFormat("ww"));
-
-        // %W - Week  number  of the current year,
-        //      starting with the first Monday as the first
-        //      day of the first week (00..53)
-        LIQUID_TO_JAVA_FORMAT.put('W', new SimpleDateFormat("ww"));
-
-        // %w - Day of the week (Sunday is 0, 0..6)
-        LIQUID_TO_JAVA_FORMAT.put('w', new SimpleDateFormat("F"));
-
-        // %x - Preferred representation for the date alone, no time
-        LIQUID_TO_JAVA_FORMAT.put('x', new SimpleDateFormat("MM/dd/yy"));
-
-        // %X - Preferred representation for the time alone, no date
-        LIQUID_TO_JAVA_FORMAT.put('X', new SimpleDateFormat("HH:mm:ss"));
-
-        // %y - Year without a century (00..99)
-        LIQUID_TO_JAVA_FORMAT.put('y', new SimpleDateFormat("yy"));
-
-        // %Y - Year with century
-        LIQUID_TO_JAVA_FORMAT.put('Y', new SimpleDateFormat("yyyy"));
-
-        // %Z - Time zone name
-        LIQUID_TO_JAVA_FORMAT.put('Z', new SimpleDateFormat("z"));
+        addDatePattern("yyyy-MM-dd HH:mm:ss");
+        addDatePattern("EEE MMM ddhh:mm:ss yyyy");
+        init();
     }
 
     /*
@@ -117,11 +55,30 @@ class Date extends Filter {
     public Object apply(Object value, Object... params) {
 
         try {
-            final long seconds = super.asString(value).equals("now") ?
-                    System.currentTimeMillis() / 1000L :
-                    Long.valueOf(super.asString(value));
+            final Long seconds;
+
+            if(super.asString(value).equals("now")) {
+                seconds = System.currentTimeMillis() / 1000L;
+            }
+            else if(super.isNumber(value)) {
+                // No need to divide this by 1000, the param is expected to be in seconds already!
+                seconds = super.asNumber(value).longValue();
+            }
+            else {
+                seconds = trySeconds(super.asString(value)); // formatter.parse(super.asString(value)).getTime() / 1000L;
+
+                if(seconds == null) {
+                    return value;
+                }
+            }
+
             final java.util.Date date = new java.util.Date(seconds * 1000L);
             final String format = super.asString(super.get(0, params));
+
+            if(format == null || format.trim().isEmpty()) {
+                return value;
+            }
+
             final java.util.Calendar calendar = java.util.Calendar.getInstance();
             calendar.setTime(date);
 
@@ -163,5 +120,136 @@ class Date extends Filter {
         catch (Exception e) {
             return value;
         }
+    }
+
+    private static void init() {
+
+        // %% - Literal ``%'' character
+        LIQUID_TO_JAVA_FORMAT.put('%', new SimpleDateFormat("%", locale));
+
+        // %a - The abbreviated weekday name (``Sun'')
+        LIQUID_TO_JAVA_FORMAT.put('a', new SimpleDateFormat("EEE", locale));
+
+        // %A - The  full  weekday  name (``Sunday'')
+        LIQUID_TO_JAVA_FORMAT.put('A', new SimpleDateFormat("EEEE", locale));
+
+        // %b - The abbreviated month name (``Jan'')
+        LIQUID_TO_JAVA_FORMAT.put('b', new SimpleDateFormat("MMM", locale));
+        LIQUID_TO_JAVA_FORMAT.put('h', new SimpleDateFormat("MMM", locale));
+
+        // %B - The  full  month  name (``January'')
+        LIQUID_TO_JAVA_FORMAT.put('B', new SimpleDateFormat("MMMM", locale));
+
+        // %c - The preferred local date and time representation
+        LIQUID_TO_JAVA_FORMAT.put('c', new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", locale));
+
+        // %d - Day of the month (01..31)
+        LIQUID_TO_JAVA_FORMAT.put('d', new SimpleDateFormat("dd", locale));
+
+        // %H - Hour of the day, 24-hour clock (00..23)
+        LIQUID_TO_JAVA_FORMAT.put('H', new SimpleDateFormat("HH", locale));
+
+        // %I - Hour of the day, 12-hour clock (01..12)
+        LIQUID_TO_JAVA_FORMAT.put('I', new SimpleDateFormat("hh", locale));
+
+        // %j - Day of the year (001..366)
+        LIQUID_TO_JAVA_FORMAT.put('j', new SimpleDateFormat("DDD", locale));
+
+        // %m - Month of the year (01..12)
+        LIQUID_TO_JAVA_FORMAT.put('m', new SimpleDateFormat("MM", locale));
+
+        // %M - Minute of the hour (00..59)
+        LIQUID_TO_JAVA_FORMAT.put('M', new SimpleDateFormat("mm", locale));
+
+        // %p - Meridian indicator (``AM''  or  ``PM'')
+        LIQUID_TO_JAVA_FORMAT.put('p', new SimpleDateFormat("a", locale));
+
+        // %S - Second of the minute (00..60)
+        LIQUID_TO_JAVA_FORMAT.put('S', new SimpleDateFormat("ss", locale));
+
+        // %U - Week  number  of the current year,
+        //      starting with the first Sunday as the first
+        //      day of the first week (00..53)
+        LIQUID_TO_JAVA_FORMAT.put('U', new SimpleDateFormat("ww", locale));
+
+        // %W - Week  number  of the current year,
+        //      starting with the first Monday as the first
+        //      day of the first week (00..53)
+        LIQUID_TO_JAVA_FORMAT.put('W', new SimpleDateFormat("ww", locale));
+
+        // %w - Day of the week (Sunday is 0, 0..6)
+        LIQUID_TO_JAVA_FORMAT.put('w', new SimpleDateFormat("F", locale));
+
+        // %x - Preferred representation for the date alone, no time
+        LIQUID_TO_JAVA_FORMAT.put('x', new SimpleDateFormat("MM/dd/yy", locale));
+
+        // %X - Preferred representation for the time alone, no date
+        LIQUID_TO_JAVA_FORMAT.put('X', new SimpleDateFormat("HH:mm:ss", locale));
+
+        // %y - Year without a century (00..99)
+        LIQUID_TO_JAVA_FORMAT.put('y', new SimpleDateFormat("yy", locale));
+
+        // %Y - Year with century
+        LIQUID_TO_JAVA_FORMAT.put('Y', new SimpleDateFormat("yyyy", locale));
+
+        // %Z - Time zone name
+        LIQUID_TO_JAVA_FORMAT.put('Z', new SimpleDateFormat("z", locale));
+    }
+
+    /**
+     * Changes the locale.
+     *
+     * @param locale the new locale.
+     */
+    public static void setLocale(Locale locale) {
+        Date.locale = locale;
+        init();
+    }
+
+    /**
+     * Adds a new Date-pattern to be used when parsing a string to a Date.
+     *
+     * @param pattern the pattern.
+     */
+    public static void addDatePattern(String pattern) {
+
+        if(pattern == null) {
+            throw new NullPointerException("date-pattern cannot be null");
+        }
+
+        datePatterns.add(pattern);
+    }
+
+    /**
+     * Removed a Date-pattern to be used when parsing a string to a Date.
+     *
+     * @param pattern the pattern.
+     */
+    public static void removeDatePattern(String pattern) {
+
+        datePatterns.remove(pattern);
+    }
+
+    /*
+     * Try to parse `str` into a Date and return this Date as seconds
+     * since EPOCH, or null if it could not be parsed.
+     */
+    private Long trySeconds(String str) {
+
+        for(String pattern : datePatterns) {
+
+            SimpleDateFormat parser = new SimpleDateFormat(pattern, locale);
+
+            try {
+                long milliseconds = parser.parse(str).getTime();
+                return milliseconds / 1000L;
+            }
+            catch(Exception e) {
+                // Just ignore and try the next pattern in `datePatterns`.
+            }
+        }
+
+        // Could not parse the string into a meaningful date, return null.
+        return null;
     }
 }
--- src/test/liqp/filters/DateTest.java
+++ src/test/liqp/filters/DateTest.java
@@ -1,11 +1,11 @@
 package liqp.filters;
 
+import java.util.Locale;
 import liqp.Template;
 import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -16,7 +16,7 @@ public class DateTest {
     public void applyTest() throws RecognitionException {
 
         final int seconds = 946702800;
-        final Date date = new Date(seconds * 1000L);
+        final java.util.Date date = new java.util.Date(seconds * 1000L);
 
         String[][] tests = {
                 {"{{" + seconds + " | date: 'mu'}}", "mu"},
@@ -52,5 +52,61 @@ public class DateTest {
 
             assertThat(rendered, is(test[1]));
         }
+    }
+
+    /*
+     * def test_date
+     *   assert_equal 'May', @filters.date(Time.parse("2006-05-05 10:00:00"), "%B")
+     *   assert_equal 'June', @filters.date(Time.parse("2006-06-05 10:00:00"), "%B")
+     *   assert_equal 'July', @filters.date(Time.parse("2006-07-05 10:00:00"), "%B")
+     *
+     *   assert_equal 'May', @filters.date("2006-05-05 10:00:00", "%B")
+     *   assert_equal 'June', @filters.date("2006-06-05 10:00:00", "%B")
+     *   assert_equal 'July', @filters.date("2006-07-05 10:00:00", "%B")
+     *
+     *   assert_equal '2006-07-05 10:00:00', @filters.date("2006-07-05 10:00:00", "")
+     *   assert_equal '2006-07-05 10:00:00', @filters.date("2006-07-05 10:00:00", nil)
+     *
+     *   assert_equal '07/05/2006', @filters.date("2006-07-05 10:00:00", "%m/%d/%Y")
+     *
+     *   assert_equal "07/16/2004", @filters.date("Fri Jul 16 01:00:00 2004", "%m/%d/%Y")
+     *
+     *   assert_equal nil, @filters.date(nil, "%B")
+     *
+     *   assert_equal "07/05/2006", @filters.date(1152098955, "%m/%d/%Y")
+     *   assert_equal "07/05/2006", @filters.date("1152098955", "%m/%d/%Y")
+     * end
+     */
+    @Test
+    public void applyOriginalTest() throws Exception {
+
+        final Filter filter = Filter.getFilter("date");
+
+        assertThat(filter.apply(seconds("2006-05-05 10:00:00"), "%B"), is((Object)"May"));
+        assertThat(filter.apply(seconds("2006-06-05 10:00:00"), "%B"), is((Object)"June"));
+        assertThat(filter.apply(seconds("2006-07-05 10:00:00"), "%B"), is((Object)"July"));
+
+        assertThat(filter.apply("2006-05-05 10:00:00", "%B"), is((Object)"May"));
+        assertThat(filter.apply("2006-06-05 10:00:00", "%B"), is((Object)"June"));
+        assertThat(filter.apply("2006-07-05 10:00:00", "%B"), is((Object)"July"));
+
+        assertThat(filter.apply("2006-07-05 10:00:00", ""), is((Object)"2006-07-05 10:00:00"));
+        assertThat(filter.apply("2006-07-05 10:00:00", null), is((Object)"2006-07-05 10:00:00"));
+
+        assertThat(filter.apply("2006-07-05 10:00:00", "%m/%d/%Y"), is((Object)"07/05/2006"));
+
+        assertThat(filter.apply("Fri Jul 16 01:00:00 2004", "%m/%d/%Y"), is((Object)"07/16/2004"));
+
+        assertThat(filter.apply(null, "%B"), is((Object)null));
+
+        assertThat(filter.apply(1152098955, "%m/%d/%Y"), is((Object)"07/05/2006"));
+        assertThat(filter.apply("1152098955", "%m/%d/%Y"), is((Object)"07/05/2006"));
+    }
+
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+
+    private static long seconds(String str) throws Exception {
+        long milliSeconds =  formatter.parse(str).getTime();
+        return milliSeconds / 1000L;
     }
 }

e18d45dacb3bb76699ddf19a8f8375e7e433f1e2
Bart Kiers <bkiers@gmail.com>
2013-03-26 23:45:56 +0100

    Added extra `cycle` unit tests.

--- src/test/liqp/tags/CycleTest.java
+++ src/test/liqp/tags/CycleTest.java
@@ -69,4 +69,88 @@ public class CycleTest {
             assertThat(rendered, is(test[1]));
         }
     }
+
+    /*
+     * def test_cycle
+     *   assert_template_result('one','{%cycle "one", "two"%}')
+     *   assert_template_result('one two','{%cycle "one", "two"%} {%cycle "one", "two"%}')
+     *   assert_template_result(' two','{%cycle "", "two"%} {%cycle "", "two"%}')
+     *
+     *   assert_template_result('one two one','{%cycle "one", "two"%} {%cycle "one", "two"%} {%cycle "one", "two"%}')
+     *
+     *   assert_template_result('text-align: left text-align: right',
+     *     '{%cycle "text-align: left", "text-align: right" %} {%cycle "text-align: left", "text-align: right"%}')
+     * end
+     */
+    @Test
+    public void cycleTest() throws Exception {
+
+        assertThat(Template.parse("{%cycle \"one\", \"two\"%}").render(), is("one"));
+        assertThat(Template.parse("{%cycle \"one\", \"two\"%} {%cycle \"one\", \"two\"%}").render(), is("one two"));
+        assertThat(Template.parse("{%cycle \"\", \"two\"%} {%cycle \"\", \"two\"%}").render(), is(" two"));
+
+        assertThat(Template.parse("{%cycle \"one\", \"two\"%} {%cycle \"one\", \"two\"%} {%cycle \"one\", \"two\"%}").render(),
+                is("one two one"));
+
+        assertThat(Template.parse("{%cycle \"text-align: left\", \"text-align: right\" %} {%cycle \"text-align: left\", \"text-align: right\"%}").render(),
+                is("text-align: left text-align: right"));
+    }
+
+    /*
+     * def test_multiple_cycles
+     *   assert_template_result('1 2 1 1 2 3 1',
+     *     '{%cycle 1,2%} {%cycle 1,2%} {%cycle 1,2%} {%cycle 1,2,3%} {%cycle 1,2,3%} {%cycle 1,2,3%} {%cycle 1,2,3%}')
+     * end
+     */
+    @Test
+    public void multiple_cyclesTest() throws Exception {
+
+        assertThat(
+                Template.parse(
+                        "{%cycle 1,2%} " +
+                        "{%cycle 1,2%} " +
+                        "{%cycle 1,2%} " +
+                        "{%cycle 1,2,3%} " +
+                        "{%cycle 1,2,3%} " +
+                        "{%cycle 1,2,3%} " +
+                        "{%cycle 1,2,3%}").render(),
+                is("1 2 1 1 2 3 1"));
+    }
+
+    /*
+     * def test_multiple_named_cycles
+     *   assert_template_result('one one two two one one',
+     *     '{%cycle 1: "one", "two" %} {%cycle 2: "one", "two" %} {%cycle 1: "one", "two" %} {%cycle 2: "one", "two" %} {%cycle 1: "one", "two" %} {%cycle 2: "one", "two" %}')
+     * end
+     */
+    @Test
+    public void multiple_named_cyclesTest() throws Exception {
+
+        assertThat(
+                Template.parse(
+                        "{%cycle 1: \"one\", \"two\" %} {%cycle 2: \"one\", \"two\" %} " +
+                        "{%cycle 1: \"one\", \"two\" %} {%cycle 2: \"one\", \"two\" %} " +
+                        "{%cycle 1: \"one\", \"two\" %} {%cycle 2: \"one\", \"two\" %}").render(),
+                is("one one two two one one"));
+    }
+
+    /*
+     * def test_multiple_named_cycles_with_names_from_context
+     *   assigns = {"var1" => 1, "var2" => 2 }
+     *   assert_template_result('one one two two one one',
+     *     '{%cycle var1: "one", "two" %} {%cycle var2: "one", "two" %} {%cycle var1: "one", "two" %} {%cycle var2: "one", "two" %} {%cycle var1: "one", "two" %} {%cycle var2: "one", "two" %}', assigns)
+     * end
+     */
+    @Test
+    public void multiple_named_cycles_with_names_from_contextTest() throws Exception {
+
+        String assigns = "{\"var1\" : 1, \"var2\" : 2 }";
+
+        assertThat(
+                Template.parse(
+                        "{%cycle var1: \"one\", \"two\" %} {%cycle var2: \"one\", \"two\" %} " +
+                        "{%cycle var1: \"one\", \"two\" %} {%cycle var2: \"one\", \"two\" %} " +
+                        "{%cycle var1: \"one\", \"two\" %} {%cycle var2: \"one\", \"two\" %}").render(assigns),
+                is("one one two two one one"));
+    }
 }

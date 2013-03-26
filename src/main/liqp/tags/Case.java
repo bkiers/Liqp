fca695d5e2f0cc2b7201995f3d1afb551873a963
Bart Kiers <bkiers@gmail.com>
2013-03-26 13:57:41 +0100

    Added some comments explaining the algorithm.

--- src/main/liqp/tags/Case.java
+++ src/main/liqp/tags/Case.java
@@ -25,12 +25,17 @@ class Case extends Tag {
             LNode node = nodes[i];
 
             if(i == nodes.length - 1 && node instanceof BlockNode) {
+                // this must be the trailing (optional) else-block
                 return node.render(context);
             }
             else {
 
                 boolean hit = false;
 
+                // Iterate through the list of terms (of which we do not know the size):
+                // - term (',' term)*
+                // - term ('or' term)*
+                // and stop when we encounter a BlockNode
                 while(!(node instanceof BlockNode)) {
 
                     Object whenExpressionValue = node.render(context);
@@ -38,6 +43,7 @@ class Case extends Tag {
                     if (LValue.areEqual(condition, whenExpressionValue)) {
                         hit = true;
                     }
+
                     i++;
                     node = nodes[i];
                 }

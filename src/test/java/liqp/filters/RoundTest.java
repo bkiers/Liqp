863a6cc3c3d88964b23880d10ad4db3a2835b325
Bart Kiers <bkiers@gmail.com>
2018-03-06 16:24:47 +0100

    Added round filter

--- src/test/java/liqp/filters/RoundTest.java
+++ src/test/java/liqp/filters/RoundTest.java
@@ -14,10 +14,6 @@ public class RoundTest {
           assert_template_result "5", "{{ input | round }}", 'input' => 4.6
           assert_template_result "4", "{{ '4.3' | round }}"
           assert_template_result "4.56", "{{ input | round: 2 }}", 'input' => 4.5612
-          assert_raises(Liquid::FloatDomainError) do
-            assert_template_result "4", "{{ 1.0 | divided_by: 0.0 | round }}"
-          end
-
           assert_template_result "5", "{{ price | round }}", 'price' => NumberLikeThing.new(4.6)
           assert_template_result "4", "{{ price | round }}", 'price' => NumberLikeThing.new(4.3)
         end
@@ -26,13 +22,20 @@ public class RoundTest {
     public void applyTest() throws RecognitionException {
 
         String[][] tests = {
-                {"", ""},
+                {"{{ nil | round }}", "0", "{}"},
+                {"{{ 'MU' | round }}", "0", "{}"},
+                {"{{ input | round }}", "5", "{ \"input\": 4.6 }"},
+                {"{{ '4.3' | round }}", "4", "{}"},
+                {"{{ input | round: 2 }}", "4.56", "{ \"input\": 4.5612 }"},
+                {"{{ input | round: 2.999 }}", "4.56", "{ \"input\": 4.5612 }"},
+                {"{{ price | round }}", "5", "{ \"price\": 4.6 }"},
+                {"{{ price | round }}", "4", "{ \"price\": 4.3 }"}
         };
 
         for (String[] test : tests) {
 
             Template template = Template.parse(test[0]);
-            String rendered = String.valueOf(template.render());
+            String rendered = String.valueOf(template.render(test[2]));
 
             assertThat(rendered, is(test[1]));
         }

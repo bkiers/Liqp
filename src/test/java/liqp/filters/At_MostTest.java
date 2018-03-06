8ace82db3209278818044587ad36e8632010b252
Bart Kiers <bkiers@gmail.com>
2018-03-06 12:54:40 +0100

    Added at_most filter

--- src/test/java/liqp/filters/At_MostTest.java
+++ src/test/java/liqp/filters/At_MostTest.java
@@ -25,13 +25,20 @@ public class At_MostTest {
     public void applyTest() throws RecognitionException {
 
         String[][] tests = {
-                {"", ""},
+                {"{{ 5 | at_most:4 }}", "4", "{}"},
+                {"{{ 5 | at_most:5 }}", "5", "{}"},
+                {"{{ 5 | at_most:6 }}", "5", "{}"},
+
+                {"{{ 4.5 | at_most:5 }}", "4.5", "{}"},
+                {"{{ width | at_most:5 }}", "5", "{ \"width\": 6 }"},
+                {"{{ width | at_most:5 }}", "4", "{ \"width\": 4 }"},
+                {"{{ 5 | at_most: width }}", "4", "{ \"width\": 4 }"}
         };
 
         for (String[] test : tests) {
 
             Template template = Template.parse(test[0]);
-            String rendered = String.valueOf(template.render());
+            String rendered = String.valueOf(template.render(test[2]));
 
             assertThat(rendered, is(test[1]));
         }

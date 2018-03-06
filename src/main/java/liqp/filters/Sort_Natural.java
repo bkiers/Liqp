0c59888f77dad96dd839247144c80cfc7d1b4827
Bart Kiers <bkiers@gmail.com>
2018-03-06 18:11:29 +0100

    Added sort_natural filter

--- src/main/java/liqp/filters/Sort_Natural.java
+++ src/main/java/liqp/filters/Sort_Natural.java
@@ -1,9 +1,28 @@
 package liqp.filters;
 
+import edu.emory.mathcs.backport.java.util.Collections;
+
+import java.util.*;
+
 public class Sort_Natural extends Filter {
 
     @Override
     public Object apply(Object value, Object... params) {
-        throw new RuntimeException("TODO: " + getClass().getSimpleName());
+
+        if (!super.isArray(value)) {
+            return value;
+        }
+
+        Object[] array = super.asArray(value);
+        List<Object> list = new ArrayList<Object>(Arrays.asList(array));
+
+        Collections.sort(list, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return String.valueOf(o1).compareToIgnoreCase(String.valueOf(o2));
+            }
+        });
+
+        return list.toArray();
     }
 }
--- src/test/java/liqp/filters/Sort_NaturalTest.java
+++ src/test/java/liqp/filters/Sort_NaturalTest.java
@@ -18,13 +18,18 @@ public class Sort_NaturalTest {
     public void applyTest() throws RecognitionException {
 
         String[][] tests = {
-                {"", ""},
+                {"{{ nil | sort_natural }}", "", "{ \"x\": [] }"},
+                {"{{ false | sort_natural }}", "false", "{ \"x\": [] }"},
+                {"{{ x | sort_natural }}", "42", "{ \"x\": 42 }"},
+                {"{{ x | sort_natural }}", "", "{ \"x\": [] }"},
+                {"{{ x | sort_natural }}", "99 A00000.0001 a01.1 a04 a1 a10 ", "{ \"x\": [\"a1 \", \"A00000.0001 \", \"a01.1 \", \"a10 \", \"a04 \", \"99 \"] }"},
+                {"{{ x | sort_natural }}", "01 02 10 100 A b Cccc cccccccc d Ddd ", "{ \"x\": [\"b \", \"A \", \"Cccc \", \"cccccccc \", \"Ddd \", \"d \", \"01 \", \"02 \", \"10 \", \"100 \"] }"}
         };
 
         for (String[] test : tests) {
 
             Template template = Template.parse(test[0]);
-            String rendered = String.valueOf(template.render());
+            String rendered = String.valueOf(template.render(test[2]));
 
             assertThat(rendered, is(test[1]));
         }

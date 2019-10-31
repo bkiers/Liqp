6a1e90582672933a3bc1f75bbe91fbdb83b81dfe
Vasyl Khrystiuk <vasyl.khrystiuk@ticketmaster.com>
2019-10-31 12:02:11 +0200

    More test

--- src/test/java/liqp/filters/FilterTest.java
+++ src/test/java/liqp/filters/FilterTest.java
@@ -1,11 +1,16 @@
 package liqp.filters;
 
+import liqp.ParseSettings;
 import liqp.Template;
+import liqp.parser.Flavor;
 import org.antlr.v4.runtime.RecognitionException;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class FilterTest {
 
@@ -24,5 +29,27 @@ public class FilterTest {
         String rendered = String.valueOf(template.render());
 
         assertThat(rendered, is("<b>hi</b>"));
+    }
+
+
+    @Test
+    public void testFlavoredFilters() {
+        String templateText = "{{ ' a  b   c' | normalize_whitespace }}";
+
+        ParseSettings jekyllSettings = new ParseSettings.Builder().withFlavor(Flavor.JEKYLL).build();
+        ParseSettings defaultSettings = new ParseSettings.Builder().build();
+
+        Template template1 = Template.parse(templateText, jekyllSettings);
+
+        String res = template1.render();
+        assertEquals("a b c", res);
+
+        Template template2 = Template.parse(templateText, defaultSettings);
+        try {
+            template2.render();
+            fail();
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("no filter available named: |normalize_whitespace"));
+        }
     }
 }

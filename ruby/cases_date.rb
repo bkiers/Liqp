b4b6c54cd447fd9a695e7fe13bd7f64a4fe797d4
Vasyl Khrystiuk <h6.msangel@gmail.com>
2021-02-07 07:20:57 +0200

    'where' filter coverage for Jekyll flavor

--- ruby/cases_date.rb
+++ ruby/cases_date.rb
@@ -8,4 +8,16 @@ pp "new Time: %s" % Time.new
 pp "now Time: %s" % Time.now
 
 pp render({ "a" => Time.now }, "{{ a | date: '%Y'}}")
-pp render({ "a" => Time.now }, "{{ a.isdst }}")
+
+tn = Time.now
+t = Time.new(2007,11,1,15,25,0, "+09:00")
+t_str = t.to_s
+# target is string representation, source is iterated as collection(and so = match in "year" part)
+# pp render({"a" => [{ "time" => t }], "b" => "2007"}, "target is string representation: {{ a | where: 'time', b | map: 'time'}}")
+# pp render({"a" => [{ "time" => tn }], "b" => tn.year.to_s}, "target is string representation: {{ a | where: 'time', b | map: 'time'}}")
+pp render({"a" => t, "b" => "2007"}, "target is string representation: {{ a | where: 'time', b | map: 'time'}}")
+
+
+# source is string representation that match the date
+pp render({"a" => [{ "time" => t_str }], "b" => t}, "source is string representation: {{ a | where: 'time', b | map: 'time'}}")
+

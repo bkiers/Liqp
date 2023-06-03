b1456dc86457167a4e67e556213485cd890d3ada
Vasyl Khrystiuk <h6.msangel@gmail.com>
2023-06-04 00:04:44 +0300

    return test on its place

--- src/test/java/liqp/tags/IncludeTest.java
+++ src/test/java/liqp/tags/IncludeTest.java
@@ -47,32 +47,27 @@ public class IncludeTest {
     public void renderTest() throws RecognitionException {
 
         String source =
-                "{% assign shape = 'circle' %}{% include 'color' %}";
-//        String source =
-//                "{% assign shape = 'circle' %}\n" +
-//                "{% include 'color' %}\n" +
-//                "{% include 'color' with 'red' %}\n" +
-//                "{% include 'color' with 'blue' %}\n" +
-//                "{% assign shape = 'square' %}\n" +
-//                "{% include 'color' with 'red' %}";
+                "{% assign shape = 'circle' %}\n" +
+                "{% include 'color' %}\n" +
+                "{% include 'color' with 'red' %}\n" +
+                "{% include 'color' with 'blue' %}\n" +
+                "{% assign shape = 'square' %}\n" +
+                "{% include 'color' with 'red' %}";
 
         Template template = TemplateParser.DEFAULT.parse(source);
-//        Template template = TemplateParser.DEFAULT_LIQUID.parse(source);
 
         String rendered = template.render();
 
-        assertThat(rendered, is("color: ''\nshape: 'circle'"));
-
-//        assertThat(rendered, is("\n" +
-//                "color: ''\n" +
-//                "shape: 'circle'\n" +
-//                "color: 'red'\n" +
-//                "shape: 'circle'\n" +
-//                "color: 'blue'\n" +
-//                "shape: 'circle'\n" +
-//                "\n" +
-//                "color: 'red'\n" +
-//                "shape: 'square'"));
+        assertThat(rendered, is("\n" +
+                "color: ''\n" +
+                "shape: 'circle'\n" +
+                "color: 'red'\n" +
+                "shape: 'circle'\n" +
+                "color: 'blue'\n" +
+                "shape: 'circle'\n" +
+                "\n" +
+                "color: 'red'\n" +
+                "shape: 'square'"));
     }
 
     @Test
